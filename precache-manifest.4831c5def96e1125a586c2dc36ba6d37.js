c044ef21dcc1fcad1038b1190181f46402acdbb7
Alejandro Gemas <alejandro.gemas@gmail.com>
2020-09-24 19:53:15 +0200

    Updates

---
+++ precache-manifest.4831c5def96e1125a586c2dc36ba6d37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce4353068ea36f1aa84b804b60da000",
+    "url": "/heroes-app-viewer/index.html"
+  },
+  {
+    "revision": "3e4ce44118e857394a35",
+    "url": "/heroes-app-viewer/static/js/2.6270374c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/heroes-app-viewer/static/js/2.6270374c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e1c7b0c5c6ea0513765",
+    "url": "/heroes-app-viewer/static/js/main.f4f5c05c.chunk.js"
+  },
+  {
+    "revision": "7c6705f16c2c65dd953a",
+    "url": "/heroes-app-viewer/static/js/runtime-main.cb147b09.js"
+  }
+]);
